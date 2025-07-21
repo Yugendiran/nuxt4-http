5dc1ca55f6439209a153922779257c6a7798870b
Yugendiran <yyugi64@gmail.com>
2025-07-21 15:44:26 +0530

    chore: enhance auth middleware with cookie handling and improve index page metadata

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,10 +1,10 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: ["../src/module"],
   devtools: { enabled: true },
-  compatibilityDate: '2025-07-20',
+  compatibilityDate: "2025-07-20",
   http: {
-    accessTokenCookie: 'nexxauthAccessToken2',
-    refreshTokenCookie: 'nexxauthRefreshToken2',
-    apiUrl: 'https://api.crackaspire.com/nexxauth/api',
+    // accessTokenCookie: 'nexxauthAccessToken2',
+    // refreshTokenCookie: 'nexxauthRefreshToken2',
+    apiUrl: "https://api.crackaspire.com/nexxauth/api",
   },
-})
+});
--- src/runtime/middleware/auth.ts
+++ src/runtime/middleware/auth.ts
@@ -1,5 +1,23 @@
-import { defineNuxtRouteMiddleware, navigateTo } from "#app";
+import {
+  defineNuxtRouteMiddleware,
+  navigateTo,
+  useRuntimeConfig,
+  useCookie,
+  abortNavigation,
+} from "#app";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  console.log("[Auth Middleware] Middleware executed successfully");
+  const config = useRuntimeConfig().public.nuxtHttp;
+
+  const accessToken = useCookie(config.accessTokenCookie);
+  const refreshToken = useCookie(config.refreshTokenCookie);
+
+  if (!accessToken.value) {
+    if (refreshToken.value) {
+      return;
+    }
+
+    abortNavigation();
+    return navigateTo(config.loginPath);
+  }
 });
