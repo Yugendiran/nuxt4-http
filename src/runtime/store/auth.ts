d5f2a30b53018994944ba1d3729d205ee6f428d9
Yugendiran <yyugi64@gmail.com>
2025-07-22 11:17:44 +0530

    .

--- src/runtime/store/auth.ts
+++ src/runtime/store/auth.ts
@@ -1,24 +1,6 @@
 import { defineStore } from "pinia";
 import { useCookie, useRuntimeConfig } from "#app";
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
-const utcToLocal = (utc: string, format?: string) => {
-  return dayjs
-    .utc(utc)
-    .tz(dayjs.tz.guess())
-    .format(format || "YYYY-MM-DD hh:mm:ss A");
-};
-
-const localToUtc = (local: string, format?: string) => {
-  return dayjs(local)
-    .utc()
-    .format(format || "YYYY-MM-DD hh:mm:ss A");
-};
+import { dayjs, utcToLocal, localToUtc } from "../utils/dayjs";
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
---
+++ src/runtime/utils/dayjs.ts
@@ -0,0 +1,23 @@
+// Utility to handle dayjs imports in a way that works with Nuxt 4
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+// Extend dayjs with plugins
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export { dayjs };
+
+export const utcToLocal = (utc: string, format?: string) => {
+  return dayjs
+    .utc(utc)
+    .tz(dayjs.tz.guess())
+    .format(format || "YYYY-MM-DD hh:mm:ss A");
+};
+
+export const localToUtc = (local: string, format?: string) => {
+  return dayjs(local)
+    .utc()
+    .format(format || "YYYY-MM-DD hh:mm:ss A");
+};
